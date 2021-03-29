281714af7a81088ad412c1ae7831b683a65cc2af
Elangel <elangelshaday@gmail.com>
2021-03-29 16:56:24 +0700

    Program ShoppingList dengan Spring Repository

--- src/main/java/shoppinglist/repository/DaftarBelanjaRepo.java
+++ src/main/java/shoppinglist/repository/DaftarBelanjaRepo.java
@@ -8,9 +8,12 @@ package shoppinglist.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import shoppinglist.entity.DaftarBelanja;
 
+import java.util.List;
+
 /**
  * @author irfin
  */
 public interface DaftarBelanjaRepo extends JpaRepository<DaftarBelanja, Long>
 {
+    List<DaftarBelanja> findByTitleContaining(String judul);
 }
